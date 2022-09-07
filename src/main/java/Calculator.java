15fb46caf915bfb47648a78731ed4c811531d2c1
Jeremiah Deffendall <jddeffendal2@bsu.edu>
2022-09-07 15:49:42 -0400

    add conversion functions

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,23 @@
 public class Calculator {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+
+    public int toDecimal(Integer quaternaryNumber) {
+        int num = 0;
+        int j = 0;
+        char[] digitList = String.valueOf(quaternaryNumber).toCharArray();
+        for (int i = digitList.length-1; i >= 0; i--){
+            num += Integer.parseInt(String.valueOf(digitList[i])) * Math.pow(4, j);
+            j++;
+        }
+        return num;
+    }
+
+    public int toQuaternary(int decimalNumber) {
+        String quaternary = "";
+        while (decimalNumber > 0) {
+            quaternary += (decimalNumber % 4);
+            decimalNumber /= 4;
+        }
+        StringBuilder reverseQuaternary = new StringBuilder(quaternary).reverse();
+        return Integer.parseInt(reverseQuaternary.toString()); //return the quaternary number as integer
     }
 }
