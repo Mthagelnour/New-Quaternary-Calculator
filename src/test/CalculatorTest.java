599ee60e1c0b80637a1e34ca035da5ba6356e535
Jacob Bishop <jacobbishop1000@gmail.com>
2022-09-08 09:32:12 -0400

    Added tests for add, subtract, multiply, divide, sqrt, and square

--- src/test/CalculatorTest.java
+++ src/test/CalculatorTest.java
@@ -53,4 +53,60 @@ public class CalculatorTest {
         var result = calc.toDecimal(temp);
         assertEquals(originalInput, result);
     }
+
+    // add tests
+    @Test
+    public void addNumbers(){
+        assertEquals(2, calc.add(1, 1));
+        assertEquals(0, calc.add(0, 0));
+        assertEquals(21003, calc.add(123, 13020));
+        assertEquals(133032, calc.add(33213, 33213));
+    }
+
+    // subtract tests
+    @Test
+    public void subtractNumbers(){
+        assertEquals(0, calc.subract(0, 0));
+        assertEquals(1113, calc.subract(1113, 0));
+        assertEquals(0, calc.subract(33213, 33213));
+        assertEquals(-5, calc.subract(1, 12));
+    }
+
+    // multiply tests
+    @Test
+    public void multiplyNumbers(){
+        assertEquals(0, calc.multiply(0, 112030131));
+        assertEquals(6, calc.multiply(2, 3));
+        assertEquals(2100, calc.multiply(30, 30));
+        assertEquals(211332, calc.multiply(231, 312));
+    }
+
+    // divide tests
+    @Test
+    public void divideNumbers(){
+        assertEquals(0, calc.divide(10, 11));
+        assertEquals(1, calc.divide(10, 10));
+        assertEquals(101, calc.divide(310, 3));
+    }
+
+    @Test
+    public void divideByZero(){
+        ArithmeticException thrown = assertThrows(ArithmeticException.class, () -> {
+            calc.divide(1, 0);
+        }, "ArithmeticException was expected");
+    }
+
+    @Test
+    public void squareNumbers(){
+        assertEquals(0, calc.square(0));
+        assertEquals(1, calc.square(1));
+        assertEquals(2221, calc.square(31));
+    }
+
+    @Test
+    public void sqrtNumbers(){
+        assertEquals(0, calc.squareRoot(0));
+        assertEquals(1, calc.squareRoot(1));
+        assertEquals(31, calc.squareRoot(2221));
+    }
 }
